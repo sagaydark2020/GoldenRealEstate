6767a1b825b2b2f7022eb5427afafa5950f0229b
Sagay <sagaydark2020@users.noreply.github.com>
2021-01-10 15:55:43 +0530

    Repo Issues Fixed and merged

---
+++ todo-app-bootstrap/src/test/java/ae/goldenrealestate/bootstrap/GoldenRealEstateMain/BootstrapTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ae.goldenrealestate.bootstrap.GoldenRealEstateMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
